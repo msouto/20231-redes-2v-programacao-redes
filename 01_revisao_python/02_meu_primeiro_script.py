62a8f90b22673aeeab41868735bda3a8a2362f3d
Moisés Souto <moises.souto@gmail.com>
2023-04-05 15:14:40 -0300

    Adicionando ao script, interação com variáveis de tipos diferentes, resultando em comportamentos de saída diferentes

--- 01_revisao_python/02_meu_primeiro_script.py
+++ 01_revisao_python/02_meu_primeiro_script.py
@@ -1,5 +1,12 @@
 #Importando um módulo 
+
 import sys #utilizar este módulo, para conseguir ler a versão do sistema operacional
 
 print(sys.platform) #impressão do nome S.O.
+
+#brincando com impressão e variáveis
+print(2**100) #impressão da potência de 2 elevado a 100
+
+x="Spam!"
+print(x*8)
 
