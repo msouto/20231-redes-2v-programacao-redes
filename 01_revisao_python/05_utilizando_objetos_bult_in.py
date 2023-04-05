5dfe04cf384c4027b9e4a90b950e3f6efe5dfedd
Moisés Souto <moises.souto@gmail.com>
2023-04-05 15:55:24 -0300

    Adicionando exemplos de utilização de objetos bult in do Python

---
+++ 01_revisao_python/05_utilizando_objetos_bult_in.py
@@ -0,0 +1,22 @@
+S = ‘spam'
+len(S) #mostrar o tamanho de s
+
+#S[0]
+#S[1:3]
+#S + ‘xyz'
+#S.find(‘pa')
+#S.upper()
+#S.isalpha()
+#S.replace(‘pa’,'ca')
+
+#L = [123, ‘casa’, 3.14]
+#len(L)
+#L[0]
+#L * 2
+#L.append(‘bola’)
+
+#M = [[1,2,3],[4,5,6],[7,8,9]]
+#col2 = [n[1] for n in M]
+#D = {‘com’:’ABC', ‘num’:23}
+#D[‘num’]
+#D[‘cor’] = ’azul'
