7f3116b95e07b6875771752168c48887361866cc
Moisés Souto <moises.souto@gmail.com>
2023-04-12 15:45:11 -0300

    Utilizando operadores bitwise em python

---
+++ 01_revisao_python/09_operadores_bit_wise.py
@@ -0,0 +1,11 @@
+#Operadores Bitwise
+#Operadores que trabalham no nível de bits:
+x = 1
+x << 2     # deslocamento dois bits a esquerda
+
+print(x | 2)      # “Or" lógico bit-a-bit
+
+print(x & 1)       # “And" lógico bit-a-bit
+
+print(y = 0b1000)   # 8 em decimal   
+print(y >> 3)       # deslocamento de três bits a direita
