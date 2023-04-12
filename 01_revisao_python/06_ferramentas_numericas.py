a83853c432fc97c83d90318ae6a24f4538832e08
Moisés Souto <moises.souto@gmail.com>
2023-04-12 15:03:45 -0300

    Revisão sobre ferramentas numéricas do python

---
+++ 01_revisao_python/06_ferramentas_numericas.py
@@ -0,0 +1,14 @@
+#Ferramentas numéricas
+#Operadores de expressões
+#+, -, *, /, //, **, %, >>, etc
+
+
+#Funções matemáticas internas
+#pow, abs, round, int, hex, min, max, etc
+#Ex: >>> min(3, 2, 1, 4)
+#1
+
+#Módulos utilitários
+#random, math, etc
+#Ex: >>> math.sin(2 * math.pi / 180)
+#0.03489949670250097
