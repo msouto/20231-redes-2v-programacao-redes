7e9658690907e29089d9a9e0f51e3dda240c151c
Moisés Souto <moises.souto@gmail.com>
2023-04-12 15:35:35 -0300

    Adicionando comentários sobre manipulação de operadores de comparação

--- 01_revisao_python/08_operadores_de_comparacao.py
+++ 01_revisao_python/08_operadores_de_comparacao.py
@@ -1,17 +1,23 @@
 #Operadores de comparação
 
 #Operadores que trabalham no nível de bits:
+#atribuição
 #>>> x = 2
 #>>> y = 4
 #>>> z = 6
+
+#Comparação
 #>>> x < y
 #True
 
+#Comparação
 #>>> z < y
 #False
 
+#Comparação
 #>>> x < y < z     # Equivalente a (x < y) and (y < z)
 #True
 
+#Comparação de igualdade
 #>>> x == z
 #False
