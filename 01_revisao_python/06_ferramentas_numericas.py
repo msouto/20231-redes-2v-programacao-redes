c213c0d27ae15206b84426b7d2b642ba243092e6
Moisés Souto <moises.souto@gmail.com>
2023-04-12 15:17:32 -0300

    Utilizando ferramentas númericas de exemplo de python

--- 01_revisao_python/06_ferramentas_numericas.py
+++ 01_revisao_python/06_ferramentas_numericas.py
@@ -8,6 +8,20 @@
 #Ex: >>> min(3, 2, 1, 4)
 #1
 
+#valor absoluto de um número
+print(abs(3.141592))
+
+#valor arredonda de um número
+print(round(3.141592))
+print(round(3.141592, 2))
+print(round(3.141592, 3))
+
+#hex - imprime em hexa um número
+#representação em decimal
+print(hex(255))
+#representação em binário
+print(hex(0b11111111))
+
 #Módulos utilitários
 #random, math, etc
 #Ex: >>> math.sin(2 * math.pi / 180)
