35f008b5073e71c822b961d2362248c848f74a0a
Moisés Souto <moises.souto@gmail.com>
2023-04-12 15:32:27 -0300

    Exemplos com operadores de comparação

---
+++ 01_revisao_python/08_operadores_de_comparacao.py
@@ -0,0 +1,17 @@
+#Operadores de comparação
+
+#Operadores que trabalham no nível de bits:
+#>>> x = 2
+#>>> y = 4
+#>>> z = 6
+#>>> x < y
+#True
+
+#>>> z < y
+#False
+
+#>>> x < y < z     # Equivalente a (x < y) and (y < z)
+#True
+
+#>>> x == z
+#False
