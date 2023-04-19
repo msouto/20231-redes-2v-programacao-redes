27a05c2ec67e7610cf0ddc7c36370f16c161d279
Moisés Souto <moises.souto@gmail.com>
2023-04-19 15:30:17 -0300

    Como formatar strings

---
+++ 01_revisao_python/14_formatacao_de_strings.py
@@ -0,0 +1,22 @@
+nome = 'joao'
+
+print(f'a casa de {nome} fica em natal')
+print('a casa de %s fica em natal' % nome)
+
+#A sintaxe completa da expressão é:
+#%[(nomedachave)] [flags] [tamanho] [.precisão] código
+#Aonde:
+#(nome da chave) - Caso se utilize dicionários para os valores
+#   flags - Especifica alguns aspectos, tais como, justificação (-), sinais numéricos (+), números positivos sem sinal e negativos com sinal (espaço), preenchimento com zeros
+#   tamanho - Tamanho minimo do texto substituído
+#   precisão - Para ponto flutuante, o número de casas decimais
+#   Código - Código que representa o tipo de dado a ser substituído
+
+#formatação de strings - códigos mais comuns
+#Código	Significado
+#s	String
+#d	decimal
+#i	inteiro
+#x	hexadecimal
+#e	Ponto flutuante com expoente
+#f	Ponto flutuante
