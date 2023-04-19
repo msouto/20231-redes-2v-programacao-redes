40381ceb3eadb726e0af117ffc929d1c644e87e3
Moisés Souto <moises.souto@gmail.com>
2023-04-19 15:42:18 -0300

    Formatação de texto

--- 01_revisao_python/14_formatacao_de_strings.py
+++ 01_revisao_python/14_formatacao_de_strings.py
@@ -20,3 +20,10 @@ print('a casa de %s fica em natal' % nome)
 #x	hexadecimal
 #e	Ponto flutuante com expoente
 #f	Ponto flutuante
+
+x = 1234
+print('inteiros: ... %d ... %-6d ... %06d' % (x, x, x))
+
+
+x = 1.23456789
+print('%-6.2f | %05.2f | %+06.2f' % (x, x, x))
