8b879132086d9dbd20f389d9ebfb8c78a9632bbf
Moisés Souto <moises.souto@gmail.com>
2023-04-19 15:49:40 -0300

    Criando listas

---
+++ 01_revisao_python/15_listas.py
@@ -0,0 +1,21 @@
+#Listas
+#Listas são coleções ordenadas de objetos
+#Listas podem conter quaisquer tipos de objetos
+#Listas são Objetos Mutáveis (Podem ter seus elementos alterados)
+#Listas têm tamanho variável, são heterogêneas e arbitrariamente aninhadas
+##Pode-se adicionar ou remover elementos
+##Podem conter tipos diferentes
+##Podem conter outras listas como elementos (Equivalente aos arrays)
+
+#Lista vazia
+L = []
+L = list()
+
+#Lista com 4 itens
+L = [4, 'abc', 1.23, [9,8,7],[1,2,3,4,5]]
+print(L)
+
+#Lista de um objeto iterável
+L = list('spam')
+print(L)
+
