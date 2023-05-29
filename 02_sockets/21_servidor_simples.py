23f2066d3cb4055fa9f72d97980d3aeeb95e6408
Moisés Souto <moises.souto@gmail.com>
2023-05-29 15:30:04 -0300

    Exemplo inicial de socket TCP

---
+++ 02_sockets/21_servidor_simples.py
@@ -0,0 +1,24 @@
+import socket
+
+#informações para comunicação
+HOST = socket.gethostbyname('localhost') #127.0.0.1
+PORT = 2000 #0 - 65535
+
+tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+tcp_server_socket.bind((HOST,PORT))
+tcp_server_socket.listen()
+
+client, addr = tcp_server_socket.accept()
+print('Conexão recebida de: ', addr)
+
+while True:
+    data = client.recv(1024)
+    message = 'Recebida'
+    byte_msg = message.encode('utf-8')
+    client.send(byte_msg)
+    print('Mensagem recebida: ',data)
+    
+client.close()
+tcp_server_socket.close()
+
