97deba9fecb3fe62ba22f1c467cee3c885ab7534
Moisés Souto <moises.souto@gmail.com>
2023-06-28 15:03:14 -0300

    Exemplo de servidor binário multiplataforma

--- 02_sockets/27b_servidor_binario_simples.py
+++ 02_sockets/27b_servidor_binario_simples.py
@@ -1,29 +1,49 @@
 import socket
+import os
 
 HOST = ''
 PORT = 2000
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((HOST, PORT))
+server_socket.listen(1)
 
-try:
-    # Solicitar nome do arquivo ao usuário
-    nome_arquivo = input("Digite o nome do arquivo: ")
-    
-    # Enviar nome do arquivo
-    nome_arquivo_encoded = nome_arquivo.encode('utf-8')
-    tamanho_nome_arquivo_encoded = len(nome_arquivo_encoded).to_bytes(4, 'big')
-    sock.sendall(tamanho_nome_arquivo_encoded + nome_arquivo_encoded)
+print("Servidor iniciado. Aguardando conexões...")
 
-    # Enviar tamanho do arquivo
-    tamanho_arquivo = len(open(nome_arquivo, 'rb').read())
-    tamanho_arquivo_encoded = tamanho_arquivo.to_bytes(4, 'big')
-    sock.sendall(tamanho_arquivo_encoded)
+while True:
+    client_socket, address = server_socket.accept()
+    print(f"Conexão estabelecida com {address[0]}:{address[1]}")
 
-    # Enviar bytes do arquivo
-    with open(nome_arquivo, 'rb') as arquivo:
-        bytes_arquivo = arquivo.read()
-        sock.sendall(bytes_arquivo)
+    try:
+        # Receber tamanho do nome do arquivo
+        tamanho_nome_arquivo_bytes = client_socket.recv(4)
+        tamanho_nome_arquivo = int.from_bytes(tamanho_nome_arquivo_bytes, 'big')
 
-finally:
-    sock.close()
+        # Receber nome do arquivo
+        nome_arquivo_bytes = client_socket.recv(tamanho_nome_arquivo)
+        nome_arquivo = nome_arquivo_bytes.decode('utf-8')
+
+        # Receber tamanho do arquivo
+        tamanho_arquivo_bytes = client_socket.recv(4)
+        tamanho_arquivo = int.from_bytes(tamanho_arquivo_bytes, 'big')
+
+        # Receber bytes do arquivo
+        bytes_arquivo = b''
+        bytes_recebidos = 0
+        while bytes_recebidos < tamanho_arquivo:
+            dados = client_socket.recv(4096)
+            if not dados:
+                break
+            bytes_arquivo += dados
+            bytes_recebidos += len(dados)
+
+        # Salvar arquivo recebido
+        with open(f"copia_{nome_arquivo}", "wb") as arquivo:
+            arquivo.write(bytes_arquivo)
+
+        print(f"Arquivo '{nome_arquivo}' recebido e salvo com sucesso!")
+
+    finally:
+        client_socket.close()
+        print(f"Conexão encerrada com {address[0]}:{address[1]}")
