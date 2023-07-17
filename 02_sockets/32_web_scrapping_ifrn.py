506aed5ffd1e70980363368c851925b212c42937
Moisés Souto <moises.souto@gmail.com>
2023-07-17 15:40:58 -0300

    Corrigindo código de scrapping no site do IFRN

--- 02_sockets/32_web_scrapping_ifrn.py
+++ 02_sockets/32_web_scrapping_ifrn.py
@@ -11,17 +11,25 @@ response = requests.get(url)
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    #encontrar as noticias
-#    noticias = soup.find_all('div', class_='grid-container noticias')
-    noticias = soup.find_all('div', class_='grid-item')
+    #Ecnontrar o elemento section que contém as noticias
+    noticias_section = soup.find('section', class_='noticias')
 
-    print(noticias)
+    #Encontrar o elemento final com as noticias
+    noticias = noticias_section.find_all('div', class_='grid-item')
 
-    #ßiterar sobre as noticias e extrair informacoes de cada
+    #iterar sobre as noticias e extrair o titulo e o link de cada noticia
     for noticia in noticias:
-         titulo = noticia.find('h3').text_strip()
+        titulo = noticia.find('h3').text.strip()
+        link = noticia.find('a')['href']
+        subtitulo = noticia.find('p', class_='subtitulo')
+        subtitulo = subtitulo.text.strip() if subtitulo else ""
+        data = noticia.find('p', class_='date').text.strip()
 
-         print(f"Titulo: {titulo}")
+        print(f"Título: {titulo}")
+        print(f"Subtítulo: {subtitulo}")
+        print(f"Data: {data}")
+        print(f"Link: {link}")
+        print("-" * 50)
 
 else:
     print(f"Erro ao obter as notícias. Código de status: {response.status_code}")
