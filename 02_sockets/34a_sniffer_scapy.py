58028b44c16b9408817108ddb35871d0973d4865
Moisés Souto <moises.souto@gmail.com>
2023-07-19 15:13:00 -0300

    Exemplo de código para sniffing de rede

---
+++ 02_sockets/34a_sniffer_scapy.py
@@ -0,0 +1,37 @@
+from scapy.all import *
+
+# Dicionários para manter o contador de pacotes por IP de origem, IP de destino e tipo de protocolo
+packet_count_src_ip = {}
+packet_count_dst_ip = {}
+packet_count_protocol = {}
+
+# Função para processar os pacotes capturados
+def process_packet(packet):
+    if IP in packet:
+        src_ip = packet[IP].src
+        dst_ip = packet[IP].dst
+        protocol = packet[IP].proto
+
+        # Incrementa o contador de pacotes por IP de origem
+        if src_ip in packet_count_src_ip:
+            packet_count_src_ip[src_ip] += 1
+        else:
+            packet_count_src_ip[src_ip] = 1
+
+        # Incrementa o contador de pacotes por IP de destino
+        if dst_ip in packet_count_dst_ip:
+            packet_count_dst_ip[dst_ip] += 1
+        else:
+            packet_count_dst_ip[dst_ip] = 1
+
+        # Incrementa o contador de pacotes por tipo de protocolo
+        if protocol in packet_count_protocol:
+            packet_count_protocol[protocol] += 1
+        else:
+            packet_count_protocol[protocol] = 1
+
+        # Exibe o conteúdo do pacote
+        print(packet.summary())
+
+# Captura de pacotes em tempo real
+sniff(prn=process_packet, store=0)
